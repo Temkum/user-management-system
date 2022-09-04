99804c9d55787dbb0bde618073ea03d2323d6251
Kum Jude Tem <judekum14@gmail.com>
2022-09-04 20:35:06 +0100

    Create relationships between roles and users

--- TODO.md
+++ TODO.md
@@ -20,3 +20,9 @@ npm install sass-loader`
 ## Build the dashboard ui
 
 ## User registration - login and logout
+
+## database schema
+
+- add roles using hasMany() relationship btwn users
+- create migration `php artisan make:model Role -a`
+- create a link table which allows us to link our roles in many to many relationship `php artisan make:migration create_role_user_table`
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,44 +1,45 @@
 @extends('templates.main')
 
 @section('content')
-    <h1 class="">register page</h1>
-
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
-                value="{{ old('name') }}" aria-describedby="name" name="name">
-            @error('name')
-                <span class="invalid-feedback" role="alert">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Email</label>
-            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
-                value="{{ old('email') }}" id="exampleInputEmail1" aria-describedby="emailHelp">
-            @error('email')
-                <span class="invalid-feedback" role="alert">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1" class="form-label">Password</label>
-            <input type="password" class="form-control @error('password') is-invalid @enderror" name="password"
-                id="exampleInputPassword1" value="{{ old('password') }}">
-            @error('password')
-                <span class="invalid-feedback" role="alert">{{ $message }}</span>
-            @enderror
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputPassword1"
-                class="form-label @error('password_confirmation') is-invalid @enderror">Confirm
-                Password</label>
-            <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
-                value="{{ old('password_confirmation') }}">
-            @error('password_confirmation')
-                <span class="invalid-feedback" role="alert">{{ $message }}</span>
-            @enderror
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+    <h1 class="">Register</h1>
+    <div class="form-width center-item">
+        <form method="POST" action="{{ route('register') }}">
+            @csrf
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                    value="{{ old('name') }}" aria-describedby="name" name="name">
+                @error('name')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Email</label>
+                <input type="email" class="form-control @error('email') is-invalid @enderror" name="email"
+                    value="{{ old('email') }}" id="exampleInputEmail1" aria-describedby="emailHelp">
+                @error('email')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Password</label>
+                <input type="password" class="form-control @error('password') is-invalid @enderror" name="password"
+                    id="exampleInputPassword1" value="{{ old('password') }}">
+                @error('password')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputPassword1"
+                    class="form-label @error('password_confirmation') is-invalid @enderror">Confirm
+                    Password</label>
+                <input type="password" class="form-control" name="password_confirmation" id="password_confirmation"
+                    value="{{ old('password_confirmation') }}">
+                @error('password_confirmation')
+                    <span class="invalid-feedback" role="alert">{{ $message }}</span>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
 @endsection
